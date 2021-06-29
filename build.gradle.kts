f43000366d8bce2d6c258ad22cb1abf1ee43fbb0
Daltin Loomis <loomisdf@gmail.com>
2021-06-28 21:53:00 -0500

    use date of current puzzle date in NYT app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,56 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.1"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.10"
-	kotlin("plugin.spring") version "1.5.10"
-	id("org.jetbrains.kotlin.plugin.jpa") version "1.5.10"
+    id("org.springframework.boot") version "2.5.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.10"
+    kotlin("plugin.spring") version "1.5.10"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.5.10"
 }
 
 group = "xyz.crossward"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "16"
+}
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// Spring
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
 
-	// Database
-	implementation("org.xerial:sqlite-jdbc:3.34.0")
+    // Database
+    implementation("org.xerial:sqlite-jdbc:3.34.0")
 
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	// Test
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-property-jvm:4.6.0")
+    testImplementation("io.mockk:mockk:1.11.0")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "16"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "16"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/xyz/crossward/service/EntryService.kt
+++ src/main/kotlin/xyz/crossward/service/EntryService.kt
@@ -5,6 +5,11 @@ import xyz.crossward.entities.Entry
 import xyz.crossward.exception.BadRequestException
 import xyz.crossward.repository.EntryRepository
 import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.TextStyle
+import java.util.*
 
 @Service
 class EntryService(
@@ -19,17 +24,62 @@ class EntryService(
      * @param entry Entry object to insert into the database
      */
     fun recordEntry(entry: Entry): Entry {
+        val puzzleDate: LocalDate = if (entry.date != null) {
+            if (isValidPuzzleDate(entry.date))
+                entry.date
+            else
+                throw BadRequestException(
+                    "Date ${entry.date} is not a valid puzzle date. " +
+                            "Time in New York: ${ZonedDateTime.now(ZoneId.of("US/Eastern"))}"
+                )
+        } else {
+            // use current puzzle date
+            getPuzzleDate(ZonedDateTime.now(ZoneId.of("US/Eastern")))
+        }
+
         val savedEntry = Entry(
             userId = entry.userId,
             time = entry.time,
-            date = entry.date ?: LocalDate.now()
+            date = puzzleDate
         )
-
-        if (entryRepository.count(savedEntry.userId, savedEntry.date) > 0) {
-            throw BadRequestException("an entry for the date ${savedEntry.date} already exists for user id ${savedEntry.userId}")
-        }
 
         entryRepository.save(savedEntry)
         return savedEntry
     }
+
+    /**
+     * Valid dates are any date before or on the current puzzle date. Nothing in the future is allowed
+     */
+    private fun isValidPuzzleDate(date: LocalDate): Boolean {
+        val currentPuzzleDate = getPuzzleDate(ZonedDateTime.now(ZoneId.of("US/Eastern")))
+        return !date.isAfter(currentPuzzleDate)
+    }
+
+    /**
+     * Get the current NYT puzzle date.
+     *
+     * Crosswords are available at the following times (in EST):
+     * Monday: 6PM (Sunday) - 10PM (Monday)
+     * Tuesday - Friday: 10PM (previous day) - 10PM
+     * Saturday: 10PM (Friday) - 6PM
+     * Sunday: 6PM (Saturday) - 6PM
+     */
+    fun getPuzzleDate(currentDate: ZonedDateTime): LocalDate =
+        when (currentDate.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US)) {
+            in listOf("Mon", "Tue", "Wed", "Thu", "Fri") ->
+                if (currentDate.toLocalTime().isAfter(LocalTime.parse("22:00:00"))) {
+                    currentDate.plusDays(1).toLocalDate()
+                } else {
+                    currentDate.toLocalDate()
+                }
+            in listOf("Sat", "Sun") ->
+                if (currentDate.toLocalTime().isAfter(LocalTime.parse("18:00:00"))) {
+                    currentDate.plusDays(1).toLocalDate()
+                } else {
+                    currentDate.toLocalDate()
+                }
+            else ->
+                // Return the current date if for whatever reason the code gets here
+                currentDate.toLocalDate()
+        }
 }
