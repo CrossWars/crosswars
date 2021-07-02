84758277951a8f674f66bb286600b9944712b6ff
Daltin <loomisdf@gmail.com>
2021-07-01 21:10:08 -0500

    use the correct version of the kotlin stdlib (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
