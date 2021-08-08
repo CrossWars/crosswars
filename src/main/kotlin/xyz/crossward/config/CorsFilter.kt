347b1a86a3da8ae4f543b1bb6e079935e1e61f6a
Daltin Loomis <loomisdf@gmail.com>
2021-08-08 08:51:59 -0700

    add cors filter

---
+++ src/main/kotlin/xyz/crossward/config/CorsFilter.kt
@@ -0,0 +1,50 @@
+package xyz.crossward.config
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import xyz.crossward.exception.UnauthorizedException
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class SimpleCORSFilter : Filter {
+
+    private val allowedOrigins = listOf("http://localhost:3000", "https://crosswars.xyz")
+
+    @Throws(ServletException::class)
+    override fun init(fc: FilterConfig?) {
+    }
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(
+        req: ServletRequest,
+        resp: ServletResponse,
+        chain: FilterChain
+    ) {
+        val response = resp as HttpServletResponse
+        val request = req as HttpServletRequest
+
+        if (req.getHeader("Origin") !in allowedOrigins) {
+            throw UnauthorizedException("Unauthorized")
+        }
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader(
+            "Access-Control-Allow-Headers",
+            "x-requested-with, authorization, Content-Type, Authorization, credential, Accept, x-internal-access-token"
+        )
+        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, resp)
+        }
+    }
+
+    override fun destroy() {}
+}
