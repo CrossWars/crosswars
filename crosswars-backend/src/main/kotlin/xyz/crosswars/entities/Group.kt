2751ededede2bbfa8dce1fa3c23c8a0e567f97eb
ElRoberto13 <rwetzler779@gmail.com>
2022-02-06 17:08:49 -0500

    Fix sql syntax error

--- crosswars-backend/src/main/kotlin/xyz/crosswars/entities/Group.kt
+++ crosswars-backend/src/main/kotlin/xyz/crosswars/entities/Group.kt
@@ -8,7 +8,7 @@ import javax.persistence.Table
 @Entity
 @Table(name = "CROSSGROUP")
 data class Group(
-        @Id val id: String,
+        @Id @Column(name = "group_id") val id: String,
         @Column val name: String,
         @Column(name = "created_by") val createdBy: String?
 )
