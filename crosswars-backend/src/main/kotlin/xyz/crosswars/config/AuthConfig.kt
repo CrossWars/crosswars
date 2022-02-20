e4d76de2c10249ddfa5d958dfea43f45f1eea2f1
ElRoberto13 <rwetzler779@gmail.com>
2022-02-20 02:16:35 -0500

    Undo committed auth key

--- crosswars-backend/src/main/kotlin/xyz/crosswars/config/AuthConfig.kt
+++ crosswars-backend/src/main/kotlin/xyz/crosswars/config/AuthConfig.kt
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationProperties(prefix = "auth")
 class AuthConfig(
     var enabled: Boolean = true,
-    var googleClientId: String = "474023833184-4odp7ummj7puclc850bo80brkcj10k4j.apps.googleusercontent.com",
-    var authKey: String = "CyAoCucPLH189AiR-Z59fG89"
+    var googleClientId: String = "",
+    var authKey: String = ""
 )
